3470e970cae636132539c0c613d62ece1185baec
ccollie <gboyah@gmail.com>
2026-04-07 23:31:48 -0400

    fix compilation

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,6 +1,7 @@
 pub mod command_parser;
 mod fanout_codec;
 mod label_search_utils;
+mod promql_utils;
 mod ts_add;
 mod ts_addbulk;
 mod ts_alter;
@@ -35,6 +36,8 @@ mod ts_queryindex;
 mod ts_queryindex_fanout_command;
 mod ts_range;
 mod utils;
+mod ts_queryrange;
+mod ts_query;
 
 // Command handlers are registered through the `#[valkey_module_macros::command]` attribute on
 // each `ts_*_cmd` function (see the individual `ts_*` modules), so they no longer need to be
