febfa5b7e18e837b00eb442bbc83ec10cc04411e
ccollie <gboyah@gmail.com>
2026-04-09 11:17:31 -0400

    cargo fmt

--- src/promql/functions/mod.rs
+++ src/promql/functions/mod.rs
@@ -17,20 +17,18 @@ mod types;
 pub(crate) mod utils;
 
 pub(crate) use crate::promql::exec::aggregations::*;
-pub(crate) use types::*;
 pub(crate) use function_list::*;
+pub(crate) use types::*;
 
 // Return the concrete `PromQLFunctionImpl` so callers can store the concrete
 // implementation without relying on opaque `impl Trait` return types.
 pub(in crate::promql) fn resolve_function(name: &str) -> Option<PromQLFunctionImpl> {
-    PromQLFunctionImpl::from_name(name).or_else(
-        || {
-            // handle holt_winters as alias
-            if name.len() == 12 && name.eq_ignore_ascii_case("holt_winters") {
-                PromQLFunctionImpl::from_name("doubvle_exponential_smoothing")
-            } else {
-                None
-            }
+    PromQLFunctionImpl::from_name(name).or_else(|| {
+        // handle holt_winters as alias
+        if name.len() == 12 && name.eq_ignore_ascii_case("holt_winters") {
+            PromQLFunctionImpl::from_name("doubvle_exponential_smoothing")
+        } else {
+            None
         }
-    )
+    })
 }
