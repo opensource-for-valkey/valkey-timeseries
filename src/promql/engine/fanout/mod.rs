6947a40501f0409590245c327cc3d544b326987e
ccollie <gboyah@gmail.com>
2026-04-18 15:31:32 -0400

    feat(promql): remove unused VectorSelectorFanoutCommand registration from fanout commands

--- src/promql/engine/fanout/mod.rs
+++ src/promql/engine/fanout/mod.rs
@@ -2,17 +2,14 @@ mod query_fanout_command;
 mod query_range_fanout_command;
 mod query_utils;
 mod type_conversions;
-mod vector_selector_fanout_command;
 
 pub(in crate::promql) use query_fanout_command::QueryFanoutCommand;
 pub(in crate::promql) use query_range_fanout_command::QueryRangeFanoutCommand;
 use valkey_module::ValkeyResult;
-pub(in crate::promql) use vector_selector_fanout_command::VectorSelectorFanoutCommand;
 
 use crate::fanout::register_fanout_operation;
 
 pub(crate) fn register_fanout_commands() -> ValkeyResult<()> {
-    register_fanout_operation::<VectorSelectorFanoutCommand>()?;
     register_fanout_operation::<QueryFanoutCommand>()?;
     register_fanout_operation::<QueryRangeFanoutCommand>()?;
     Ok(())
